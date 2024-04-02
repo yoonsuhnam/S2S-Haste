bda0cd30978f97aa4292049f6a4b49cc785e3995
Noobuhl <akaashpatel9@gmail.com>
2024-04-01 20:12:39 -0400

    ur smellu

---
+++ Assets/drawerCode.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawerCode : MonoBehaviour
+{
+    Animator myAnimator;
+
+    const String  PRESS_ANIM = "openD";
+    //const String  RED_ANIM = "red";
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.GetKeyDown(KeyCode.Space)){
+            myAnimator.SetTrigger(RED_ANIM);
+        }*/
+    }
+
+    private void OnMouseDown(){
+        myAnimator.SetTrigger(PRESS_ANIM);
+    }
+}
+
+
